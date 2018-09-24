48752e4986e0f11b9ba78beecc773cefa1b2dcb6
EMMANUEL VELAZQUEZ <rl200331@rlas-macair-32.rlas-116.org>
2018-09-24 11:01:29 -0500

    Initial Commit

---
+++ githubSample2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  githubSample2
+//
+//  Created by EMMANUEL VELAZQUEZ on 9/24/18.
+//  Copyright © 2018 EMMANUEL VELAZQUEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
